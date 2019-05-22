196dcd1881298f03c5c1269c42534c215058e63e
lovely <ldde07@gmail.com>
2019-05-22 14:37:45 -0700

    Get post from journal page

--- themes/redstarter/front-page.php
+++ themes/redstarter/front-page.php
@@ -7,28 +7,51 @@
 
 get_header();?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<section class="home-logo">	
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-full.svg" alt="Inhabitent Logo">
-			</section>
-			<section class="shop-stuff">
-				<h2>Shop Stuff</h2>
-			</section>
-			<section class="inhabitent-journal">
-						<h2>Inhabitent Journal</h2>
-			</section>
-			<section class="latest-adventures">
-						<h2>Latest Adventure</h2>
-			</section>
-			<?php while (have_posts()): the_post();?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<section class="front-page-hero">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-full.svg" alt="Inhabitent Logo">
+		</section>
 
-					<?php get_template_part('template-parts/content', 'page');?>
+		<section class="shop-container">
+			<h2>Shop Stuff</h2>
+		</section>
 
-				<?php endwhile; // End of the loop. ?>
+		<section class="fp-journal">
+			<div class="journal-container">
+				<h2>Inhabitent Journal</h2>
+				<ul>
+			<?php $args = array('post_type' => 'post', 'posts_per_page' => 3, 'order' => 'ASC');
+			$journal_posts = get_posts($args); // returns an array of posts
+			?>
+			<?php foreach ($journal_posts as $post): setup_postdata($post);?>
+				<li>
+				<!-- Post thumbnail -->
+                <?php if ( has_post_thumbnail() ) { ?>
+                    <div class="thumbnail-wrapper">
+                    	<?php the_post_thumbnail( 'large' ); ?>
+                    </div>
+				<?php } ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<div class="post-text">	
+					<div class="fp-post-meta">
+                    	<?php echo get_the_date(); ?> 
+                   		<?php echo get_comments_number(); ?> 
+                    </div>
+				
+				<!-- Post title -->
+                    <a href="<?php  echo get_the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+                    <a class="read-more-btn" href="<?php echo get_the_permalink(); ?>">Read Entry</a>
+				</div>
 
-<?php get_sidebar();?>
+				</li>
+			<?php endforeach;
+
+wp_reset_postdata();?>
+</ul>
+				</div><!-- #end of journal-container -->
+			</section><!-- #end of fp-journal -->
+	</main><!-- #main -->
+</div><!-- #primary -->
+
 <?php get_footer();?>
--- themes/redstarter/header.php
+++ themes/redstarter/header.php
@@ -21,21 +21,22 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="nav-container">
-				<div class="site-branding">
-				<div class="site-logo">
-				<a href="home.php" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-tent-white.svg"></a>
-				
-				</div>
+
+					<div class="site-branding">
+						<div class="site-logo">
+							<a href="home.php" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-tent-white.svg"></a>
+						</div>
 					
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
+					<!-- <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
+						<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button> -->
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 
 				</nav><!-- #site-navigation -->
+
 				</div><!-- .nav-container -->
 			</header><!-- #masthead -->
 
