b9ee62369b786a72d718c334ca1a9ae66a440edc
lovely <ldde07@gmail.com>
2019-05-22 17:39:06 -0700

    Center the post in the front page

--- themes/redstarter/front-page.php
+++ themes/redstarter/front-page.php
@@ -9,14 +9,38 @@ get_header();?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+		<!-- banner section -->
 		<section class="front-page-hero">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-full.svg" alt="Inhabitent Logo">
 		</section>
 
+		<!-- shop section -->
 		<section class="shop-container">
 			<h2>Shop Stuff</h2>
+		<?php
+$terms = get_terms(array('taxonomy' => 'product-type', 'hide_empty' => 0)
+);
+?>
+			<div class="product-terms">
+			<?php
+foreach ($terms as $term):
+?>
+				<div class ="product-term">
+					<img src="<?php echo get_template_directory_uri() . '/assets/images/product-type-icons/' . $term->slug . '.svg' ?>" />
+					<p><?php echo $term->description; ?></p>
+					<p>
+						<a class="link" href="<?php echo get_term_link($term); ?>">
+						<?php echo $term->name ?> Stuff
+						</a>
+					</p>
+				</div><!--end of product terms-->
+<?php
+endforeach;
+?>
+				</div><!--end of product term-->
 		</section>
 
+		<!--Journal Section-->
 		<section class="fp-journal">
 			<div class="journal-container">
 				<h2>Inhabitent Journal</h2>
@@ -25,33 +49,43 @@ get_header();?>
 			$journal_posts = get_posts($args); // returns an array of posts
 			?>
 			<?php foreach ($journal_posts as $post): setup_postdata($post);?>
-				<li>
-				<!-- Post thumbnail -->
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <div class="thumbnail-wrapper">
-                    	<?php the_post_thumbnail( 'large' ); ?>
-                    </div>
-				<?php } ?>
+					<li>
+					<!-- Post thumbnail -->
+					<?php if (has_post_thumbnail()) {?>
+						<div class="thumbnail-wrapper">
+						<?php the_post_thumbnail('large');?>
+						</div>
+						
+						<?php }?>
+						<div class="post-text">
+						<div class="fp-post-meta">
+						<?php echo get_the_date(); ?>
+						<?php echo get_comments_number(); ?>
+						</div>
 
-				<div class="post-text">	
-					<div class="fp-post-meta">
-                    	<?php echo get_the_date(); ?> 
-                   		<?php echo get_comments_number(); ?> 
-                    </div>
-				
-				<!-- Post title -->
-                    <a href="<?php  echo get_the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-                    <a class="read-more-btn" href="<?php echo get_the_permalink(); ?>">Read Entry</a>
-				</div>
-
-				</li>
-			<?php endforeach;
+						<!-- Post title -->
+						<a href="<?php echo get_the_permalink(); ?>"><h3><?php the_title();?></h3></a>
+						<a class="read-more-btn" href="<?php echo get_the_permalink(); ?>">Read Entry</a>
+						</div>
+						</li>
+						<?php endforeach;
 
 wp_reset_postdata();?>
 </ul>
 				</div><!-- #end of journal-container -->
 			</section><!-- #end of fp-journal -->
+
+	<section class="adventure-section">
+			<h2>Latest Adventures</h2>
+	</section>
+
+
+
+
+
 	</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_footer();?>
+
+
--- themes/redstarter/header.php
+++ themes/redstarter/header.php
@@ -35,6 +35,18 @@
 					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button> -->
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 
+<div class="header-search">
+	<?php get_search_form();?>
+</div>
+
+
+
+
+
+
+
+
+
 				</nav><!-- #site-navigation -->
 
 				</div><!-- .nav-container -->
